686826d58ec403bec42cdb7af6480c09658921bb
gauravThakur-sgl <gaurav.thakur@shipglobal.in>
2025-02-25 11:40:32 +0530

    removed checkbox condition of billing and shipping address from order Slice

--- src/app/features/order/orderSlice.ts
+++ src/app/features/order/orderSlice.ts
@@ -36,18 +36,6 @@ const orderSlice = createSlice({
 
     toggleCheckbox: (state) => {
       state.isChecked = !state.isChecked;
-      if (state.isChecked) {
-        state.consigneeDetail.billingfirstName = state.consigneeDetail.firstName;
-        state.consigneeDetail.billinglastName = state.consigneeDetail.lastName;
-        state.consigneeDetail.billingmobileNumber = state.consigneeDetail.mobileNumber;
-        state.consigneeDetail.billingCountry = state.consigneeDetail.country;
-        state.consigneeDetail.billingLandMark = state.consigneeDetail.landMark;
-        state.consigneeDetail.billingAddress1 = state.consigneeDetail.address1;
-        state.consigneeDetail.billingAddress2 = state.consigneeDetail.address2;
-        state.consigneeDetail.billingcity = state.consigneeDetail.shippingcity;
-        state.consigneeDetail.billingPincode = state.consigneeDetail.shippingPincode;
-        state.consigneeDetail.billingState = state.consigneeDetail.shippingState;
-      }
     },
     nextStep: (state) => {
       state.currentStep += 1;
@@ -55,11 +43,8 @@ const orderSlice = createSlice({
     prevStep: (state) => {
       state.currentStep -= 1;
     },
-    setStep: (state, action: PayloadAction<number>) => {
-      state.currentStep = action.payload;
-    },
   },
 });
 
-export const { setFormData, toggleCheckbox, nextStep, prevStep, setStep } = orderSlice.actions;
+export const { setFormData, toggleCheckbox, nextStep, prevStep } = orderSlice.actions;
 export default orderSlice.reducer;
